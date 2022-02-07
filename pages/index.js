696b2d4aebf3e07d88bc04dab4c5a6f874bba724
Mohammad <mohammad.hooshdar@yahoo.com>
2022-02-07 22:56:59 +0330

    chore: minor changes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world Mohammad Hooshdar
       </h1>
 
       <footer>
